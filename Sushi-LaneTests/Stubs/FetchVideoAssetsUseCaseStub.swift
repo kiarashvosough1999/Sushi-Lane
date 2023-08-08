b2eb1f6abfab6e053c00d9670c7bfa200e4f62d7
Kiarash Vosough <vosough.k@gmail.com>
2023-08-08 18:42:32 +0330

    implemented FetchVideoAssetsUseCaseStub

---
+++ Sushi-LaneTests/Stubs/FetchVideoAssetsUseCaseStub.swift
@@ -0,0 +1,35 @@
+//
+//  FetchVideoAssetsUseCaseStub.swift
+//  Sushi-LaneTests
+//
+//  Created by Kiarash Vosough on 8/8/23.
+//
+
+import Foundation
+@testable import Sushi_Lane
+
+final class FetchVideoAssetsUseCaseStub {
+
+    private let error: Error?
+    private let delayInSeconds: UInt64
+    private let assets: [VideoAssetEntity]
+    
+    init(
+        error: Error? = nil,
+        delayInSeconds: UInt64 = 0,
+        assets: [VideoAssetEntity] = []
+    ) {
+        self.error = error
+        self.delayInSeconds = delayInSeconds
+        self.assets = assets
+    }
+}
+
+extension FetchVideoAssetsUseCaseStub: FetchVideoAssetsUseCaseProtocol {
+
+    func fetch() async throws -> [VideoAssetEntity] {
+        try await Task.sleep(nanoseconds: delayInSeconds * NSEC_PER_SEC)
+        if let error { throw error }
+        return assets
+    }
+}
