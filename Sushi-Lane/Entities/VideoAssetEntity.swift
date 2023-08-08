681b3472692d19eb4c943c1c33a69fd9f00e006e
Kiarash Vosough <45785128+kiarashvosough1999@users.noreply.github.com>
2023-08-08 18:15:54 +0330

    Feature !2: added entites

---
+++ Sushi-Lane/Entities/VideoAssetEntity.swift
@@ -0,0 +1,14 @@
+//
+//  VideoAssetEntity.swift
+//  Sushi-Lane
+//
+//  Created by Kiarash Vosough on 8/8/23.
+//
+
+import Foundation
+
+struct VideoAssetEntity {
+    let imageURL: String
+    let titleDefault: String
+    let tvShow: TVShowEntity
+}
